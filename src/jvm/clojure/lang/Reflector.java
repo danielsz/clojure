dedc98827e42a138efa5e5f9bb30f902dbb0be6d
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2020-08-11 03:11:53 +0300

    Bytecoder patch
    - https://github.com/mirkosertic/Bytecoder/issues/438

--- src/jvm/clojure/lang/Reflector.java
+++ src/jvm/clojure/lang/Reflector.java
@@ -12,8 +12,8 @@
 
 package clojure.lang;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
+
+
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -24,36 +24,16 @@ import java.util.stream.Collectors;
 
 public class Reflector{
 
-private static final MethodHandle CAN_ACCESS_PRED;
 
-// Java 8 is oldest JDK supported
-private static boolean isJava8() {
-	return System.getProperty("java.vm.specification.version").equals("1.8");
-}
-
-static {
-	MethodHandle pred = null;
-	try {
-		if (! isJava8())
-			pred = MethodHandles.lookup().findVirtual(Method.class, "canAccess", MethodType.methodType(boolean.class, Object.class));
-	} catch (Throwable t) {
-		Util.sneakyThrow(t);
-	}
-	CAN_ACCESS_PRED = pred;
-}
+// Java 8 is not supported
 
 private static boolean canAccess(Method m, Object target) {
-	if (CAN_ACCESS_PRED != null) {
-		// JDK9+ use j.l.r.AccessibleObject::canAccess, which respects module rules
-		try {
-			return (boolean) CAN_ACCESS_PRED.invoke(m, target);
-		} catch (Throwable t) {
-			throw Util.sneakyThrow(t);
-		}
-	} else {
-		// JDK 8
-		return true;
-	}
+
+    try {
+	return (boolean) m.canAccess(target);
+    } catch (Throwable t) {
+	throw Util.sneakyThrow(t);
+    }
 }
 
 private static Collection<Class> interfaces(Class c) {
